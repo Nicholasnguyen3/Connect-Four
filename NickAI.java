34a2dcf3d087e9ba678bb4d7a6557513df926128
Nicholasnguyen3 <d805nick@gmail.com>
2026-01-07 14:02:44 -0800

    Create NickAI.java

---
+++ NickAI.java
@@ -0,0 +1,33 @@
+package hw4;
+
+import java.util.Random;
+import hw4.CFPlayer;
+import hw4.CFGame;
+
+public class NickAI implements CFPlayer {
+
+    // picks a move for the AI without actually playing it
+    public int nextMove(CFGame g){
+        Random rand = new Random();
+        int column;
+        boolean valid_move = false;
+        
+        // checks to see if there is a winning move that can be played
+        column = g.winning_move();
+        
+        // if no winning move can be played find a random valid move
+        if (column == 8) {
+            while (valid_move == false) {
+                column = rand.nextInt(7);
+                valid_move = g.is_valid(column);
+            }
+        }
+        
+        return column;
+    }
+    
+    //return name of AI
+    public String getName() {
+        return "Nick's AI";
+    };
+}
